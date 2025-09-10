45fe6e719fe1890324cc83ecb267a6695ce359c0
hebem <hebemilanesio1@gmail.com>
2025-09-09 21:19:09 -0300

    Subida de archivos para conectar BD global PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,30 @@
 import { Module } from '@nestjs/common';
-
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ImcModule } from './module/imc/imc.module';
 import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ImcEntity } from './module/imc/entities/imc.entity';
+import * as dotenv from 'dotenv';
 
+dotenv.config(); // carga variables del .env
 
 @Module({
-  imports: [ImcModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST ,
+      port: parseInt(process.env.DB_PORT || '5432'), // Default to 5432 if DB_PORT is undefined
+      username: process.env.DB_USER ,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [ImcEntity],
+      synchronize: false,
+      ssl: {
+        rejectUnauthorized: false, // esto permite conexión SSL sin certificado verificado
+      },
+    }),
+    ImcModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
