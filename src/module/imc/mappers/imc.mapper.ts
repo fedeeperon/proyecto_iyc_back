68037941fd34cbd80d18de399d43adc2f9fc7348
hebemilanesio1 <134965584+hebemilanesio1@users.noreply.github.com>
2025-09-11 17:21:26 -0300

    imc.mapper.ts vinculado a create-imc.dto.ts

--- src/module/imc/mappers/imc.mapper.ts
+++ src/module/imc/mappers/imc.mapper.ts
@@ -1,19 +1,18 @@
 import { ImcEntity } from '../entities/imc.entity';
-import { ImcDto } from '../dto/imc.dto';
+import { CreateImcDto } from '../dto/create-imc.dto';
 
 export class ImcMapper {
-    static toDto(entity: ImcEntity): ImcDto {
-        const dto = new ImcDto();
-        dto.id = entity.id;
+    static toCreateDto(entity: ImcEntity): CreateImcDto {
+        const dto = new CreateImcDto();
         dto.peso = Number(entity.peso);
         dto.altura = Number(entity.altura);
         dto.imc = Number(entity.imc);
-        dto.categoria = entity.categoria; // ahora es del tipo Categoria correcto
+        dto.categoria = entity.categoria;
         dto.fecha = entity.fecha;
         return dto;
     }
 
-    static toDtoList(entities: ImcEntity[]): ImcDto[] {
-        return entities.map(entity => this.toDto(entity));
+    static toCreateDtoList(entities: ImcEntity[]): CreateImcDto[] {
+        return entities.map(entity => this.toCreateDto(entity));
     }
 }
