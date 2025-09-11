6fd7196fce51ad6bb2b4e0b0f5f75af827937a70
hebemilanesio1 <134965584+hebemilanesio1@users.noreply.github.com>
2025-09-11 17:09:34 -0300

    Update imc.entity.ts

--- src/module/imc/entities/imc.entity.ts
+++ src/module/imc/entities/imc.entity.ts
@@ -1,23 +1,25 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
-import { Categoria } from '../enum/categoria-column.enum';
+import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert } from 'typeorm';
 
 @Entity('imc')
 export class ImcEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column('numeric', { precision: 5, scale: 2 })
+  @Column('float')
   peso: number;
 
-  @Column('numeric', { precision: 3, scale: 2 })
+  @Column('float')
   altura: number;
 
-  @Column('numeric', { precision: 5, scale: 3 })
+  @Column('float')
   imc: number;
 
-  @Column({ type: 'enum', enum: Categoria })
-  categoria: Categoria;
+  @Column()
+  categoria: string;
 
-  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  @Column({ type: 'timestamp' })
   fecha: Date;
+
+  
 }
+
